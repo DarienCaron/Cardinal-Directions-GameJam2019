8b3d687512cf8d2df1ff712060c5b42868413865
Darien Caron <caro0103@algonquinlive.com>
2019-01-02 15:24:58 -0500

    Initial Commit

---
+++ Cardinal Directions/Assets/_Scripts/CompassHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompassHandler : MonoBehaviour
+{
+
+    public GameObject Player;
+
+    private void Update()
+    {
+
+
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, Player.transform.localEulerAngles.y));
+        
+    }
+}
---
+++ Cardinal Directions/Assets/_Scripts/FlightScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlightScript : MonoBehaviour
+{
+
+
+
+    public float RotationSpeedZ = 3;
+    public float RotationSpeedX = 3;
+
+    public Vector3 LocalRotation;
+
+    public float MinAngle = -45;
+    public float MaxAngle = 45;
+
+    public float FlightSpeed = 45;
+
+    public float Gravity = 9.8f;
+
+    Vector3 LocalVelocity;
+
+    Rigidbody _rigidBody;
+
+    private void Awake()
+    {
+       
+    }
+
+    void FixedUpdate ()
+    {
+
+
+        
+       
+
+        float Pitch = Input.GetAxis("Vertical"); // Speed variable
+        float Roll = -Input.GetAxis("Horizontal") * 3; // Speed Variable
+        transform.Rotate( Pitch, 0, Roll);
+
+        
+
+        transform.position = LocalVelocity;
+
+        
+        
+    }
+}
