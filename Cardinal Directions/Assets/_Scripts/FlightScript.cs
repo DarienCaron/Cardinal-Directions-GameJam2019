f5e8ef5ab3354ebc2b60a928f5cddf6a9c454f36
Darien Caron <caro0103@algonquinlive.com>
2019-01-02 21:11:45 -0500

    Added the proper flight
    Need to fix up the flying. It now flies similar to a bird

--- Cardinal Directions/Assets/_Scripts/FlightScript.cs
+++ Cardinal Directions/Assets/_Scripts/FlightScript.cs
@@ -15,35 +15,62 @@ public class FlightScript : MonoBehaviour
     public float MinAngle = -45;
     public float MaxAngle = 45;
 
-    public float FlightSpeed = 45;
+    public float FlightSpeed = 0;
 
     public float Gravity = 9.8f;
+
+    public float MaxSpeed = 25;
+
+    public float WingRotationSpeed = 50;
 
     Vector3 LocalVelocity;
 
     Rigidbody _rigidBody;
 
+    float YRotation = 0;
+    float XRotation = 0;
+    float ZRotation = 0;
+
+    public bool Landed { get; set; }
     private void Awake()
     {
-       
+        _rigidBody = GetComponent<Rigidbody>();
     }
 
     void FixedUpdate ()
     {
+        if(!Landed)
+        {
+            Vector3 ForwardDirection = transform.forward;
+
+            ForwardDirection.Normalize();
+
+            YRotation += Input.GetAxis("Horizontal") * Time.deltaTime * WingRotationSpeed;
+            XRotation += -Input.GetAxis("Vertical") * Time.deltaTime * WingRotationSpeed;
+            ZRotation += -Input.GetAxis("Horizontal") * Time.deltaTime * WingRotationSpeed;
 
 
-        
+            XRotation = Mathf.Clamp(XRotation, MinAngle, MaxAngle);
+            ZRotation = Mathf.Clamp(ZRotation, MinAngle, MaxAngle);
+
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(XRotation, YRotation, ZRotation), Time.deltaTime * WingRotationSpeed);
+
+            //transform.rotation = Quaternion.Euler(XRotation, YRotation, ZRotation);
+
+
+
+            Debug.Log(ForwardDirection);
+
+            transform.Translate(ForwardDirection * Time.deltaTime * FlightSpeed, Space.World);
+        }
+    }
+
+
+    public void Flap()
+    {
        
-
-        float Pitch = Input.GetAxis("Vertical"); // Speed variable
-        float Roll = -Input.GetAxis("Horizontal") * 3; // Speed Variable
-        transform.Rotate( Pitch, 0, Roll);
-
-        
-
-        transform.position = LocalVelocity;
-
-        
         
     }
+    
 }
---
+++ Cardinal Directions/Assets/_Scripts/Nest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nest : MonoBehaviour {
+
+    public Transform NestRestLocation;
+    public SphereCollider Collider;
+
+
+    private void Awake()
+    {
+        if(Collider)
+        {
+            Debug.Log("Collider is set");
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.GetComponent<FlightScript>())
+        {
+            other.gameObject.GetComponent<FlightScript>().Landed = true;
+            other.gameObject.transform.position = NestRestLocation.position;
+            other.gameObject.transform.rotation = NestRestLocation.rotation;
+            other.gameObject.GetComponent<Rigidbody>().freezeRotation = true;
+            other.gameObject.GetComponent<Rigidbody>().MovePosition(NestRestLocation.position);
+            other.gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
+            
+        }
+    }
+}
